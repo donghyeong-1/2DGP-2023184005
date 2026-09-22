a732d293dd12898588df12c30eeea2d872798602
donghyeong-1 <ohamam67@gmail.com>
2026-09-22 22:32:15 +0900

    원형 이동 함수에 캐릭터 그리기 추가

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -8,7 +8,9 @@ character = load_image('character.png')
 
 def move_circle():
     print("CIRCLE")
-    
+    clear_canvas()
+    character.draw(400, 300)
+    update_canvas()
     pass
 
 def move_rectangle():

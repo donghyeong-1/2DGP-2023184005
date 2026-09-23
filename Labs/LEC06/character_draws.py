4bc272fef2abc575b0eff09c736f77e4a9843cf0
donghyeong-1 <ohamam67@gmail.com>
2026-09-23 13:19:32 +0900

    원형 경로를 따라 캐릭터 이동 구현

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -7,10 +7,14 @@ character = load_image('character.png')
 
 
 def move_circle():
+    for degree in range(360):
+        theta = math.radians(degree)
+        x = 400 + 200 * math.cos(theta)
+        y = 300 + 200 * math.sin(theta)
 
-    clear_canvas()
-    character.draw(400, 300)
-    update_canvas()
+        clear_canvas()
+        character.draw(x, y)
+        update_canvas()
     pass
 
 def move_rectangle():
